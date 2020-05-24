1016fd61066105f7b6a5c0c9193ee2d844efd30d
Dirk Bosman <dirkjobosman@gmail.com>
2020-05-24 22:17:49 +0200

    More CSS added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,94 @@
-import React, {useState} from "react";
+import "./style.css";
+import image from "./sky.jpg";
+
+import React, { useState } from "react";
+
 import ReactDom from "react-dom";
 
-const initialState = [1,2,3];
+const initialState = ["milk", "eggs", "bread"];
 
-// create App component
+// arrow function.
 const App = () => {
-    
-    // "todos"-house with "setTodos"-function as door to edit/add/delete into house.
-    const [todos, setTodos]  = useState(initialState);
+  const [todos, setTodos] = useState(initialState);
+  const [editIndex, setEditIndex] = useState(-1);
 
-    const addItem = () => {
-        setTodos([...todos, todos.length+1]);
-    };
-    
-    // we need to use "map" and not "foreach" or "for" to change the [1,2,3]- object.
-    const todosMarkup = todos.map((item) => <li key={item}>{item}</li>);
+  const addItem = () => {
+    // setTodos([...todos, parseInt(todos[todos.length - 1]) + 1]);
+    setTodos([...todos, "(your new item)"]);
+  };
 
-    return (
-        <div>
-            <button onClick={addItem}>Add</button>
-            {todosMarkup}
-        </div>
-    );
-}
+  const todosMarkup = todos.map((item, index) => (
+    <li className="list" key={index}>
+      <div className="item">{item}</div>
+      <button
+        className="button_1"
+        onClick={() => {
+          setEditIndex(index);
+        }}
+      >
+        Edit
+      </button>
+      <button
+        className="button_2"
+        onClick={() => {
+          const newTodos = todos.filter((item, idx) => {
+            // return all the items which do not match the current index
+            return index !== idx;
+          });
+          setTodos(newTodos);
+        }}
+      >
+        Delete
+      </button>
+      {editIndex === index && (
+        <EditTodo
+          todoitem={item}
+          onUpdate={(newTodo) => {
+            const editedTodos = todos.map((item, index) => {
+              if (index === editIndex) {
+                return newTodo;
+              }
+              return item;
+            });
+            setEditIndex(-1);
+            setTodos(editedTodos);
+          }}
+        />
+      )}
+    </li>
+  ));
 
+  // should return only one node
+  return (
+    <div>
+      <button className="button_3" onClick={addItem}>
+        Add
+      </button>
+      {todosMarkup}
+    </div>
+  );
+};
+
+const EditTodo = ({ todoitem, onUpdate }) => {
+  const [todoText, setTodoText] = useState(todoitem);
+
+  const handleNewText = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        className="item_text_box"
+        type="text"
+        value={todoText}
+        onChange={handleNewText}
+      />
+      <button className="button_4" onClick={() => onUpdate(todoText)}>
+        Save
+      </button>
+    </div>
+  );
+};
 
 ReactDom.render(<App />, document.getElementById("app"));
